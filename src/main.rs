4b4c8487a42e20784016d09195551f7fff372de6
Michael McCune <elmiko@redhat.com>
2022-02-15 16:09:12 -0500

    add full logic to find_must_gather_root
    This adds the recursive search function and also changes the return type
    to an Option<PathBuf>.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -11,7 +12,11 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    let mgpath = find_must_gather_root(cli.path).unwrap();
+    let mgpath = match find_must_gather_root(cli.path) {
+        Some(path) => path,
+        None => panic!("Cannot determine root of must-gather"),
+    };
+
     println!("must-gather path: {:?}", mgpath);
 }
 
@@ -24,14 +29,26 @@ fn main() {
 /// 3. if there is a single subdirectory in the path, recursively run this function on it and
 ///    return the result.
 /// 4. return an error
-fn find_must_gather_root(path: String) -> Result<PathBuf, String> {
+fn find_must_gather_root(path: String) -> Option<PathBuf> {
+    let orig = PathBuf::from(&path);
     let vpath: PathBuf = [String::from(&path), String::from("version")].iter().collect();
     let npath: PathBuf = [String::from(&path), String::from("namespaces")].iter().collect();
     let csrpath : PathBuf = [String::from(&path), String::from("cluster-scoped-resources")].iter().collect();
 
     if vpath.is_file() || (npath.is_dir() && csrpath.is_dir()) {
-        return Ok(PathBuf::from(path))
+        return Some(orig)
     }
 
-    Err(String::from("No must-gather root found"))
+    let directories: Vec<PathBuf> = fs::read_dir(orig).unwrap()
+        .into_iter()
+        .filter(|r| r.is_ok())
+        .map(|r| r.unwrap().path())
+        .filter(|r| r.is_dir())
+        .collect();
+
+    if directories.len() == 1 {
+        find_must_gather_root(String::from(directories[0].to_str().unwrap()))
+    } else {
+        None
+    }
 }
