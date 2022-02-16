a12986ae1c410e27aa656334e02b51332f465af0
Michael McCune <elmiko@redhat.com>
2022-02-15 21:10:54 -0500

    change basename to title in template

--- src/templates.rs
+++ src/templates.rs
@@ -3,11 +3,11 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate<'a> {
-    basename: &'a str,
+    title: &'a str,
 }
 
-pub fn build_index_template(basename: &str) -> IndexTemplate {
+pub fn build_index_template(title: &str) -> IndexTemplate {
     IndexTemplate {
-        basename: basename,
+        title: title,
     }
 }
