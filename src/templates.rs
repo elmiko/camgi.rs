f59a953736694bf6b0a90e96ecd024af6b38bc91
Michael McCune <elmiko@redhat.com>
2022-02-16 09:48:25 -0500

    format code with cargo fmt

--- src/templates.rs
+++ src/templates.rs
@@ -7,7 +7,5 @@ pub struct IndexTemplate<'a> {
 }
 
 pub fn build_index_template(title: &str) -> IndexTemplate {
-    IndexTemplate {
-        title: title,
-    }
+    IndexTemplate { title: title }
 }
