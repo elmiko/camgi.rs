15f29af3c0595bf431c9e8bcb600c212e1a54cc7
Michael McCune <elmiko@redhat.com>
2022-03-04 17:36:57 -0500

    refactor html methods to better align with lib usage

--- src/html.rs
+++ src/html.rs
@@ -9,15 +9,13 @@ pub struct Html {
 
 impl Html {
     pub fn from(mustgather: MustGather) -> Result<Html> {
-        let mut html = Html {
-            buffer: Buffer::new(),
-        };
+        let mut buffer = Buffer::new();
+        let mut html = buffer.html().attr("lang='en'");
 
-        html.add_html();
-        html.add_head(&mustgather)?;
-        html.add_body(&mustgather)?;
+        add_head(&mut html, &mustgather)?;
+        add_body(&mut html, &mustgather)?;
 
-        Ok(html)
+        Ok(Html { buffer: buffer })
     }
 }
 
@@ -27,61 +25,58 @@ impl Html {
     }
 }
 
-impl Html {
-    fn add_body(&mut self, mustgather: &MustGather) -> Result<()> {
-        let mut html = self.buffer.html();
-        let mut body = html.body();
+fn add_body(parent: &mut Node, mustgather: &MustGather) -> Result<()> {
+    let mut body = parent.body();
 
-        // main div
-        let mut app = body
-            .div()
-            .attr("id=\"app\"")
-            .attr("class=\"container-fluid\"");
-        let mut row = app.div().attr("class=\"row mt-2\"");
-        let mut nav = row
-            .div()
-            .attr("class=\"col-2\"")
-            .attr("id=\"nav-col\"")
-            .div()
-            .attr("class=\"list-group\"");
-        row.div()
-            .attr("class=\"col-10\"")
-            .div()
-            .attr("id=\"main-content\"")
-            .attr("class=\"overflow-auto\"")
-            .span()
-            .attr("v-html=\"content\"");
+    // main div
+    let mut app = body
+        .div()
+        .attr("id=\"app\"")
+        .attr("class=\"container-fluid\"");
+    let mut row = app.div().attr("class=\"row mt-2\"");
 
-        // scripts
-        body.script()
-            .attr("src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js\"")
-            .attr("integrity=\"sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf\"")
-            .attr("crossorigin=\"anonymous\"");
-        body.script()
-            .attr("src=\"https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js\"");
-        body.script()
-            .write_str(include_str!("files/index_script.js"))?;
+    // nav list
+    let mut nav = row.div().attr("class=\"col-2\"").attr("id=\"nav-col\"");
+    let mut navlist = nav.div().attr("class=\"list-group\"");
+    navlist
+        .a()
+        .attr("href=\"#\"")
+        .attr("v-on:click=\"changeContent('summary')\"")
+        .attr("class=\"list-group-item list-group-item-action\"")
+        .write_str("Summary")?;
 
-        Ok(())
-    }
+    // content
+    let mut content = row.div().attr("class=\"col-10\"");
+    content
+        .div()
+        .attr("id=\"main-content\"")
+        .attr("class=\"overflow-auto\"")
+        .span()
+        .attr("v-html=\"content\"");
 
-    fn add_head(&mut self, mustgather: &MustGather) -> Result<()> {
-        let mut html = self.buffer.html();
-        let mut head = html.head();
-        writeln!(head.title(), "{}", mustgather.title())?;
-        head.meta().attr("charset='utf-8'");
-        head.link()
-            .attr("href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css\"")
-            .attr("rel=\"stylesheet\"")
-            .attr("integrity=\"sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6\"")
-            .attr("crossorigin=\"anonymous\"");
-        head.style()
-            .write_str(include_str!("files/index_style.css"))?;
-        Ok(())
-    }
+    // scripts
+    body.script()
+        .attr("src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js\"")
+        .attr("integrity=\"sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf\"")
+        .attr("crossorigin=\"anonymous\"");
+    body.script()
+        .attr("src=\"https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js\"");
+    body.script()
+        .write_str(include_str!("files/index_script.js"))?;
 
-    fn add_html(&mut self) -> Result<()> {
-        self.buffer.html().attr("lang='en'");
-        Ok(())
-    }
+    Ok(())
+}
+
+fn add_head(parent: &mut Node, mustgather: &MustGather) -> Result<()> {
+    let mut head = parent.head();
+    head.title().write_str(mustgather.title().as_str())?;
+    head.meta().attr("charset='utf-8'");
+    head.link()
+        .attr("href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css\"")
+        .attr("rel=\"stylesheet\"")
+        .attr("integrity=\"sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6\"")
+        .attr("crossorigin=\"anonymous\"");
+    head.style()
+        .write_str(include_str!("files/index_style.css"))?;
+    Ok(())
 }
