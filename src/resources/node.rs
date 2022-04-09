95ad504125dd774f917ea56144a90e1f7eba626c
michael mccune <msm@opbstudios.com>
2022-04-08 22:17:41 -0400

    refactor node to use condition function

--- src/resources/node.rs
+++ src/resources/node.rs
@@ -3,7 +3,6 @@
 
 use crate::prelude::*;
 use crate::resources::Resource;
-use yaml_rust::Yaml;
 
 #[derive(Debug, Clone)]
 pub struct Node {
@@ -13,7 +12,7 @@ pub struct Node {
 
 impl Resource for Node {
     fn from(manifest: Manifest) -> Node {
-        let ready = is_ready_condition(&manifest);
+        let ready = manifest.has_condition_status("Ready", "True");
         Node { manifest, ready }
     }
 
@@ -31,40 +30,5 @@ impl Resource for Node {
 
     fn safename(&self) -> &String {
         &self.manifest.safename
-    }
-}
-
-fn is_ready_condition(manifest: &Manifest) -> bool {
-    let empty = Vec::<Yaml>::new();
-    let conditions = manifest.as_yaml()["status"]["conditions"]
-        .as_vec()
-        .unwrap_or(&empty);
-    for c in conditions {
-        if c["type"].as_str().unwrap() == "Ready" && c["status"].as_str().unwrap() == "False" {
-            return false;
-        }
-    }
-    true
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ::std::path::PathBuf;
-
-    #[test]
-    fn test_node_is_ready_condition_true() {
-        let manifest = Manifest::from(PathBuf::from(
-            "testdata/must-gather-valid/sample-openshift-release/cluster-scoped-resources/core/nodes/ip-10-0-0-1.control.plane.yaml"
-        )).unwrap();
-        assert_eq!(is_ready_condition(&manifest), true)
-    }
-
-    #[test]
-    fn test_node_is_ready_condition_false() {
-        let manifest = Manifest::from(PathBuf::from(
-            "testdata/must-gather-valid/sample-openshift-release/cluster-scoped-resources/core/nodes/ip-10-0-0-2.control.plane.yaml"
-        )).unwrap();
-        assert_eq!(is_ready_condition(&manifest), false)
     }
 }
